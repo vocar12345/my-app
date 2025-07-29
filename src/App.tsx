dd4a2832bb786a1375dab28b54a05fe84fc5771a
Vuk Micic <vukimicic@gmail.com>
2025-07-29 14:50:26 +0200

    set up development enviorment, react+vite, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-blue-500">
+      Hello, Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
+
+
+
