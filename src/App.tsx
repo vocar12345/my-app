9a277a2e358841fb34309c98e3478b831c1db02a
Vuk Micic <vukimicic@gmail.com>
2025-07-29 15:24:23 +0200

    installed browser router for routing, made structure for public and private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import SignInForm from './_auth/forms/SignInForm'
+import SignUpForm from './_auth/forms/SignUpForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-blue-500">
-      Hello, Tailwind is working!
-    </div>
-  );
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
 }
 
-export default App;
+export default App
+
 
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 const container = document.getElementById('root')
 if (!container) throw new Error('Root container missing in index.html')
 
 createRoot(container).render(
+  <BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>,
+  </BrowserRouter>
 )
 
